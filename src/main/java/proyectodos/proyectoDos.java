9874ad1e374bf1deeeb5962f5b678d270924618a
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-04-27 18:34:56 +0200

    68542

--- src/main/java/proyectodos/proyectoDos.java
+++ src/main/java/proyectodos/proyectoDos.java
@@ -19,26 +19,34 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 
-public class proyectoDos implements Initializable{
-    @FXML AnchorPane izquierdo;
-    @FXML AnchorPane derecho;
+public class proyectoDos implements Initializable {
+    @FXML
+    AnchorPane izquierdo;
+    @FXML
+    AnchorPane derecho;
 
-    @FXML Pane panelIzquierdo;
-    @FXML Pane panelDerecho;
+    @FXML
+    Pane panelIzquierdo;
+    @FXML
+    Pane panelDerecho;
 
-    @FXML HBox todosLosBotonesPrimeraParte;
-    @FXML HBox todosLosBotonesSegundaParte;
-    @FXML HBox hboxSuperior;
-    @FXML HBox hboxBajBox;
-    @FXML ImageView imagen;
+    @FXML
+    HBox todosLosBotonesPrimeraParte;
+    @FXML
+    HBox todosLosBotonesSegundaParte;
+    @FXML
+    HBox hboxSuperior;
+    @FXML
+    HBox hboxBajBox;
+    @FXML
+    ImageView imagen;
 
     Label palabras = new Label();
 
     String secreta = "THIAR";
-    private int fallos=1;
-    private final int MAX_FALLOS=7;
-    private ArrayList<Character>letrasPulsadas= new ArrayList<>();
-
+    private int fallos = 1;
+    private final int MAX_FALLOS = 7;
+    private ArrayList<Character> letrasPulsadas = new ArrayList<>();
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
@@ -46,7 +54,7 @@ public class proyectoDos implements Initializable{
         ponerTitulo();
         // String formaSectreto="";
         // for (int i = 0; i < secreta.length(); i++) {
-        //     formaSectreto+="_ ";
+        // formaSectreto+="_ ";
         // }
         // palabras.setFont( new Font("Ubuntu",20));
         // palabras.setText(formaSectreto.trim());
@@ -55,22 +63,25 @@ public class proyectoDos implements Initializable{
         imagen.setImage(new Image(getClass().getResourceAsStream("img/Ahorcado1.png")));
     }
 
-    @FXML private void ponerTitulo(){
+    @FXML
+    private void ponerTitulo() {
         Label texto = new Label("Etiqueta juego del ahorcado");
-        texto.setFont(new Font("Ubuntu",24));
+        texto.setFont(new Font("Ubuntu", 24));
         hboxSuperior.getChildren().add(texto);
-        //texto.setVisible(false);
+        // texto.setVisible(false);
     }
 
-    @FXML private void ponerBotones(){
-        String[] vocabulario = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Y", "Z"};
+    @FXML
+    private void ponerBotones() {
+        String[] vocabulario = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P",
+                "Q", "R", "S", "T", "U", "V", "W", "Y", "Z" };
         TeclaPulsada gestor = new TeclaPulsada();
         for (int i = 0; i < vocabulario.length; i++) {
-            if (i<=13) {
+            if (i <= 13) {
                 Button botoncito = new Button(vocabulario[i]);
                 botoncito.setOnAction(gestor);
                 todosLosBotonesPrimeraParte.getChildren().add(botoncito);
-            }else{
+            } else {
                 Button botoncito = new Button(vocabulario[i]);
                 botoncito.setOnAction(gestor);
                 todosLosBotonesSegundaParte.getChildren().add(botoncito);
@@ -78,8 +89,7 @@ public class proyectoDos implements Initializable{
         }
     }
 
-
-    class TeclaPulsada implements EventHandler<ActionEvent>{
+    class TeclaPulsada implements EventHandler<ActionEvent> {
         @Override
         public void handle(ActionEvent tecla) {
             // TODO Auto-generated method stub
@@ -89,50 +99,54 @@ public class proyectoDos implements Initializable{
         }
     }
 
-    public void comprobarLetra(String c){
-        boolean acertada=false;
-        String formatoSecreto="";
+    public void comprobarLetra(String c) {
+        boolean acertada = true;
+        String formatoSecreto = "";
 
         letrasPulsadas.add(c.charAt(0));
         if (secreta.contains(c)) {
             for (int i = 0; i < secreta.length(); i++) {
                 if (letrasPulsadas.contains(secreta.charAt(i))) {
-                    formatoSecreto=formatoSecreto+secreta.charAt(i)+" ";
-                }else{
-                    acertada=false;
-                    formatoSecreto+="_ ";
+                    formatoSecreto = formatoSecreto + secreta.charAt(i) + " ";
+                } else {
+                    acertada = false;
+                    formatoSecreto += "_ ";
                 }
             }
-            palabras.setFont( new Font("Ubuntu",20));
+            palabras.setFont(new Font("Ubuntu", 20));
             palabras.setText(formatoSecreto.trim());
             hboxBajBox.getChildren().add(palabras);
-        }else{
+        } else {
             fallos++;
-            acertada=false;
-            imagen.setImage(new Image(getClass().getResourceAsStream("img/Ahorcado"+fallos+".png")));
+            acertada = false;
+            imagen.setImage(new Image(getClass().getResourceAsStream("img/Ahorcado" + fallos + ".png")));
         }
-        //acertada
-        //!formatoSecreto.contains("_")
+        // acertada
+        // !formatoSecreto.contains("_")
         if (!formatoSecreto.contains("_")) {
-            acertada=true;
-        }
-        if (acertada) {
-            Alert alertaGanadora = new Alert(AlertType.CONFIRMATION);
-            alertaGanadora.setTitle("Has ganado.");
-            alertaGanadora.setHeaderText("Muy bien hecho.");
-            alertaGanadora.setContentText("Has adivinado la palabra secreta.");
-            alertaGanadora.showAndWait();
+            acertada();
         }
 
-        if (fallos==MAX_FALLOS) {
-            Alert alertaPerdedora = new Alert(AlertType.ERROR);
-            alertaPerdedora.setTitle("Has perdido.");
-            alertaPerdedora.setHeaderText("No has adivinado la palabra.");
-            alertaPerdedora.setContentText("No has adivinado la palabra secreta.");
-            alertaPerdedora.showAndWait();
+        if (fallos == MAX_FALLOS) {
+            fallada();
         }
+
     }
 
-    
-    
+    public void acertada() {
+        Alert alertaGanadora = new Alert(AlertType.CONFIRMATION);
+        alertaGanadora.setTitle("Has ganado.");
+        alertaGanadora.setHeaderText("Muy bien hecho.");
+        alertaGanadora.setContentText("Has adivinado la palabra secreta.");
+        alertaGanadora.showAndWait();
+    }
+
+    public void fallada() {
+        Alert alertaPerdedora = new Alert(AlertType.ERROR);
+        alertaPerdedora.setTitle("Has perdido.");
+        alertaPerdedora.setHeaderText("No has adivinado la palabra.");
+        alertaPerdedora.setContentText("No has adivinado la palabra secreta.");
+        alertaPerdedora.showAndWait();
+    }
+
 }
